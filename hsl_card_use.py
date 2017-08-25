af70b857de0bfd4169a1f705b09437c344060c67
Nathan Plamondon <nplamondon@gmail.com>
2017-08-25 13:03:48 -0700

    PEP8ify, remove unneeded calculate_card_numbers parameter

--- hsl_card_use.py
+++ hsl_card_use.py
@@ -6,11 +6,12 @@ import string
 
 def main(cardholder_file: string, door_log_file: string):
     (cardholders, door_logs) = read_files(cardholder_file, door_log_file)
-    door_log = calculate_card_numbers(door_logs, door_log_file)
+    door_log = calculate_card_numbers(door_logs)
     cardholders = calculate_stats(cardholders, door_log)
     write_files(cardholders, door_log, cardholder_file, door_log_file)
 
-def calculate_stats(cardholders, door_log):
+
+def calculate_stats(cardholders: list, door_log: list):
     card_events = [door_log[i][1] for i in range(len(door_log))]
 
     for i in range(len(cardholders)):
@@ -26,7 +27,8 @@ def calculate_stats(cardholders, door_log):
 
     return cardholders
 
-def calculate_card_numbers(door_logs, door_log_file):
+
+def calculate_card_numbers(door_logs: list):
     log = []
     for index in range(len(door_logs) - 1):
         cur_row = door_logs[index]
@@ -47,31 +49,32 @@ def read_files(cardholder_file: string, door_log_file: string):
     cardholders = []
     # name	card_number	exit_reason
     with open(cardholder_file) as f:
-        csvreader = csv.reader(f, delimiter=',')
-        for row in csvreader:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
             cardholders.append(row)
 
     door_logs = []
     # id	key	data	created_at	updated_at	card_number
     with open(door_log_file) as f:
-        csvreader = csv.reader(f, delimiter=',')
-        for row in csvreader:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
             door_logs.append(row)
 
-    return (cardholders[1:], door_logs[1:])
+    return cardholders[1:], door_logs[1:]
 
-def write_files(cardholders, door_log, cardholder_file, door_log_file):
+
+def write_files(cardholders: list, door_log: list, cardholder_file: string, door_log_file: string):
     with open(cardholder_file[:-4] + '-out.csv', 'w') as f:
-        csvwriter = csv.writer(f, delimiter=',')
-        csvwriter.writerow(['name', 'card_number', 'exit_reason', 'open_count', 'last_open_utc'])
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerow(['name', 'card_number', 'exit_reason', 'open_count', 'last_open_utc'])
         for row in cardholders:
-            csvwriter.writerow(row)
+            csv_writer.writerow(row)
 
     with open(door_log_file[:-4] + '-out.csv', 'w') as f:
-        csvwriter = csv.writer(f, delimiter=',')
-        csvwriter.writerow(['time', 'card_number'])
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerow(['time', 'card_number'])
         for row in door_log:
-            csvwriter.writerow(row)
+            csv_writer.writerow(row)
 
 
 if __name__ == '__main__':
